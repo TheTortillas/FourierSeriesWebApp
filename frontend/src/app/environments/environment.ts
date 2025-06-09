e39853cea5408f5abc436a90aff10c3318e705f1
Sebas <sebasverac331@gmail.com>
2025-06-08 21:22:44 -0600

    feat: agregar imagen y mejorar metadatos SEO en index.html para optimización de búsqueda

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    apiUrl: 'http://66.179.92.108:3000',
+    // apiUrl: 'http://66.179.92.108:3000',         // http
+    apiUrl: 'https://fouriersolver.com:443/api',    // https
 };
