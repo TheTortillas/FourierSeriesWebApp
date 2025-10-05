97fb4da5e63bd81e89f261ca5e39a3a243ed9263
Sebas <sebasverac331@gmail.com>
2025-10-05 16:54:09 -0600

    feat: enhance server configuration to support dynamic language routing and localization

--- frontend/server.ts
+++ frontend/server.ts
@@ -2,14 +2,22 @@ import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
+import { basename, dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import { LOCALE_ID } from '@angular/core';
 
-// The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
+
+  /* Get the language from the corresponding folder */
+  const lang = basename(serverDistFolder);
+
+  /* Set the route for static content and APP_BASE_HREF */
+  const langPath = `/${lang}/`;
+
+  /* Note that the 'browser' folder is located two directories above 'server/{lang}' */
+  const browserDistFolder = resolve(serverDistFolder, '../../browser/${lang}');
   const indexHtml = join(serverDistFolder, 'index.server.html');
 
   const commonEngine = new CommonEngine();
@@ -19,23 +27,26 @@ export function app(): express.Express {
 
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
+
   // Serve static files from /browser
-  server.get('**', express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: 'index.html',
-  }));
+  // Complete the route for static content by concatenating the language.
+  server.get('*.*', express.static(browserDistFolder, { maxAge: '1y' }));
 
   // All regular routes use the Angular engine
-  server.get('**', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
+  server.get('*', (req, res, next) => {
+    /* Discard baseUrl as we will provide it with langPath */
+    const { protocol, originalUrl, headers } = req;
 
     commonEngine
       .render({
         bootstrap,
         documentFilePath: indexHtml,
         url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+        publicPath: resolve(serverDistFolder, '../../browser/'),
+        providers: [
+          { provide: APP_BASE_HREF, useValue: langPath },
+          { provide: LOCALE_ID, useValue: lang },
+        ],
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
