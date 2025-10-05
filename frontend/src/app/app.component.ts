fb5a4e1e172ea70aa11cdb7e2ad8cc2f9ccb0a1c
Sebas <sebasverac331@gmail.com>
2025-10-05 17:05:14 -0600

    feat: implement localization for page title in multiple languages

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,12 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'Fourier Calculator';
+export class AppComponent implements OnInit {
+  title = $localize`:@@pageTitle:Calculadora de Series de Fourier y DFT | Herramienta de Análisis Matemático Online`;
+
+  constructor(private titleService: Title) {}
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+  }
 }
